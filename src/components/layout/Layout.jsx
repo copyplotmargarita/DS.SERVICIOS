5da2902b9a1690bfe680b5adb5b34531154dfc3a
DaxSalinas <copyplotmargarita@gmail.com>
2026-09-11 00:41:26 -0400

    Force 100vw max width and overflow-x-hidden on layout and body

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom';
 
 export function Layout() {
   return (
-    <div className="flex flex-col min-h-screen font-body-md text-on-surface bg-surface">
+    <div className="flex flex-col min-h-screen font-body-md text-on-surface bg-surface overflow-x-hidden w-full max-w-[100vw]">
       <Navbar />
       <main className="w-full pt-20 bg-surface flex-grow">
         <Outlet />
